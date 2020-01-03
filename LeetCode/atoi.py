e9ee29dc017020b548c0e0c99d08b26437e523f4
LearnerChick <sldhana@ymail.com>
2020-01-03 12:18:35 -0800

    ASCII to integer implementation

---
+++ LeetCode/atoi.py
@@ -0,0 +1,45 @@
+def atoi(inp: str) -> int:
+    min_int, max_int = (-2147483648, 2147483647)
+
+    inp = inp.strip(' ')
+    if not inp:
+        return 0
+    sign = ''
+    if inp[0] == '-':
+        sign = '-'
+        inp = inp[1:len(inp)]
+    elif inp[0] == '+':
+        inp = inp[1:len(inp)]
+
+    if not inp or not inp[0].isnumeric():
+        return 0
+
+    num = ''
+
+    for ch in inp:
+        if ch.isnumeric():
+            num += ch
+            continue
+        break
+
+    num = int(sign + num)
+
+    num = max(num, min_int)
+    num = min(num, max_int)
+
+    return num
+
+
+assert atoi('-1') == -1
+assert atoi('') == 0
+assert atoi('+6') == 6
+assert atoi('6333') == 6333
+assert atoi('4193 with words') == 4193
+assert atoi('words and 987"') == 0
+assert atoi('-91283472332') == -2147483648
+assert atoi('91283472332') == 2147483647
+
+
+
+
+
