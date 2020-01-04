962a04fe7728478c8a130a936254cfa33ea330c0
LearnerChick <sldhana@ymail.com>
2020-01-03 21:48:11 -0800

    New code for index and value retrieval

--- LeetCode/2sum.py
+++ LeetCode/2sum.py
@@ -2,7 +2,7 @@
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 """
 
-def two_sum(values:list, target:int) -> tuple:
+def two_sum_indices(values:list, target:int) -> tuple:
     store = {}
 
     for i in range(0, len(values)):
@@ -14,8 +14,27 @@ def two_sum(values:list, target:int) -> tuple:
     return None
 
 
-assert(two_sum([1, 4, 8, 3, 2, 9, 15], 13)) == (1, 5)
-assert(two_sum([2, 7, 11, 15], 9)) == (0, 1)
-assert(two_sum([2, 7, 11, 15], 8)) is None
-assert(two_sum([2, 7, 11, 15], 26)) == (2, 3)
-assert(two_sum([2, 11, -11, 15], 0)) == (1, 2)
+assert(two_sum_indices([1, 4, 8, 3, 2, 9, 15], 13)) == (1, 5)
+assert(two_sum_indices([2, 7, 11, 15], 9)) == (0, 1)
+assert(two_sum_indices([2, 7, 11, 15], 8)) is None
+assert(two_sum_indices([2, 7, 11, 15], 26)) == (2, 3)
+assert(two_sum_indices([2, 11, -11, 15], 0)) == (1, 2)
+
+
+def two_sum_numbers(values:list, target:int) -> tuple:
+    store = {}
+
+    for i in range(0, len(values)):
+        store[values[i]] = i
+        needed = target - values[i]
+        if needed in store:
+            item = needed, values[i]
+            return item
+    return None
+
+
+assert(two_sum_numbers([1, 4, 8, 3, 2, 9, 15], 13)) == (4, 9)
+assert(two_sum_numbers([2, 7, 11, 15], 9)) == (2, 7)
+assert(two_sum_numbers([2, 7, 11, 15], 8)) is None
+assert(two_sum_numbers([2, 7, 11, 15], 26)) == (11, 15)
+assert(two_sum_numbers([2, 11, -11, 15], 0)) == (11, -11)
